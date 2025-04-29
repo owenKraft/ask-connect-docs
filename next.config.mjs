1c62b7cbacb00352b06e2da6f704111b87315cc4
Owen Kraft <owenkraft7@gmail.com>
2025-04-28 22:46:54 -0500

    remove frame-ancestors

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   //     },
   //   ];
   // },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
